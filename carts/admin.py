3ae3f88e3c6753a8840b2859871dfc51ea9aa61c
OlmosBenito <ben.olmos@duocuc.cl>
2022-05-08 23:40:22 -0400

    pagina cart con carrito de compras

---
+++ carts/admin.py
@@ -0,0 +1,8 @@
+from operator import imod
+from django.contrib import admin
+from .models import Cart, CartItem
+
+# Register your models here.
+
+admin.site.register(Cart)
+admin.site.register(CartItem)
